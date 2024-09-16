0a293747a19b7fcdd8aac01aca9aa96066b7cf4b
mahammadov724 <nerimanmehemmedov@gmail.com>
2024-09-16 19:53:25 +0400

    ders5

---
+++ codes/javase/JavaSeCode/src/ders5lesson2/ders5lesson2.java
@@ -0,0 +1,35 @@
+package ders5lesson2;
+
+public class ders5lesson2 {
+
+	public static void main(String[] args) {
+		 int intValue = 55;
+	     byte byteValue = (byte) intValue;
+	     System.out.println(byteValue);
+
+	     short shortValue1 = 636;
+	     byte byteValue1 = (byte) shortValue1;
+	     System.out.println(byteValue1);
+	     
+	     long longValue2 = 458;
+	     short shortValue2 = (short) longValue2;
+	     System.out.println(shortValue2);
+	     
+	     long longValue3 = 92523635483L;
+	     int intValue3 = (int) longValue3;
+	     System.out.println(intValue3);
+	     
+	     double doubleValue4 = 65787.3;
+	     int intValue4 = (int) doubleValue4;
+	     System.out.println(intValue4);
+	        
+	     char charValue5 = 'q';
+	     int intValue5 = (int) charValue5;
+	     System.out.println(intValue5);
+
+	     int intValue6 = 266;
+	     char charValue6 = (char) intValue6;
+	     System.out.println(charValue6);
+	}
+
+}
