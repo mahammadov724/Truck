abe8151942fe95bd88285ad53114c6f19ac79525
mahammadov724 <nerimanmehemmedov@gmail.com>
2024-10-23 19:11:55 +0400

    Update ArraysDemoHome.java

--- codes/javase/JavaSeCode/src2/ders15/ArraysDemoHome.java
+++ codes/javase/JavaSeCode/src2/ders15/ArraysDemoHome.java
@@ -1,16 +1,59 @@
 package ders15;
 
 import java.util.Arrays;
+import java.util.Iterator;
 
 public class ArraysDemoHome {
 
 	public static void main(String[] args) {
 		int[] numbers = {1,6,4,8};
-		Arrays.sort(numbers);
-		System.out.println(Arrays.toString(numbers));
+		int sum = 0;
+        for (int number : numbers) {
+            sum += number;
+        }
+        System.out.println("Massivin elementlərinin cəmi: " + sum);
 		
-		int index = Arrays.binarySearch(numbers, 8);
-		System.out.println(index);
+        System.out.println("Massivin elementləri (foreach):");
+		for (int number : numbers) {
+			System.out.println(number);
+		}
+		
+		double ƏdədiOrta = (double) sum / numbers.length;
+		System.out.println("Massivin ədədi ortası: " + ƏdədiOrta);
+		
+		
+		int min = numbers[0];
+        int max = numbers[0];
+        for (int number : numbers) {
+            if (number < min) {
+                min = number;
+            }
+            if (number > max) {
+                max = number;
+            }
+        }
+        System.out.println("Massivin ən kiçik elementi: " + min);
+        System.out.println("Massivin ən böyük elementi: " + max);
+		
+        
+        System.out.println("Tək ədədlər:");
+        for (int number : numbers) {
+            if (number % 2 != 0) {
+                System.out.println(number);
+            }
+        }
+
+        System.out.println("Cüt ədədlər:");
+        for (int number : numbers) {
+            if (number % 2 == 0) {
+                System.out.println(number);
+            }
+        }
+        
+        System.out.println("Sıralanmış massiv (artan):");
+        for (int number : numbers) {
+            System.out.print(number + " ");
+        }
 	}
 
 }
