5ebd9d2468c092369a9a88a390135fc05a218d85
mahammadov724 <nerimanmehemmedov@gmail.com>
2024-10-29 20:12:10 +0400

    Ders17

---
+++ codes/javase/JavaSeCode/src2/ders17/Main.java
@@ -0,0 +1,14 @@
+package ders17;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Car myCar = new Car("BMW", 2020);
+        myCar.start();
+        myCar.accelerate(340);
+        myCar.brake(30);
+        myCar.displayInfo();
+        myCar.brake(40);
+	}
+
+}
