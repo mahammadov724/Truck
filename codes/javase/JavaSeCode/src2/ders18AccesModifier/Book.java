a3a14117387e50877dff598fbf3b013bd35620c0
mahammadov724 <nerimanmehemmedov@gmail.com>
2024-11-01 18:50:52 +0400

    ders18

---
+++ codes/javase/JavaSeCode/src2/ders18AccesModifier/Book.java
@@ -0,0 +1,14 @@
+package ders18AccesModifier;
+
+public class Book {
+	
+	public String title;
+    public String author;
+    public String id;
+    public Book(String title, String author, String id) {
+        this.title = title;
+        this.author = author;
+        this.id = id;
+    }
+
+}
---
+++ codes/javase/JavaSeCode/src2/ders18AccesModifiers/Other/MainOther.java
@@ -0,0 +1,18 @@
+package ders18AccesModifiers.Other;
+
+import ders18AccesModifier.Book;
+
+public class MainOther {
+
+	public static void main(String[] args) {
+		 Book myBook = new Book("1984", "George Orwell", "978-0451524935");
+	        myBook.title = "Animal Farm";
+	        myBook.author = "George Orwell";
+
+	        System.out.println("Book Title: " + myBook.title);
+	        System.out.println("Book Author: " + myBook.author);
+	        System.out.println("Book Id: " + myBook.id);
+
+	}
+
+}
