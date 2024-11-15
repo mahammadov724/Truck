aa9023e50d0cf398c54f5d851e34c35a7239b441
mahammadov724 <nerimanmehemmedov@gmail.com>
2024-11-15 14:30:22 +0400

    Ders7

---
+++ codes/javase/JavaSeCode/src2/ders7/Enum.java
@@ -0,0 +1,35 @@
+package ders7;
+
+import java.util.Scanner;
+
+enum Yay {
+	Iyun,
+	Iyul,
+	Avqust,
+}
+public class Enum {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Yayın İçindəki Ayları Yaz!");
+		String ay = scan.nextLine().toLowerCase();
+		switch (ay) {
+		case "iyul": {
+			System.out.println("Düzdür");
+			break;
+		}
+		case "iyun": {
+			System.out.println("Düzdür");
+			break;
+		}
+		case "avqust": {
+			System.out.println("Düzdür");
+			break;
+		}
+		default:
+			System.out.println("Səhvdir!");
+		}
+
+	}
+
+}
