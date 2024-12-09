523adcdcfc3a9a33246f8f071b1877b1fd2ab1e8
mahammadov724 <nerimanmehemmedov@gmail.com>
2024-12-09 17:14:23 +0400

    Ders14

---
+++ codes/javase/JavaSe/src/ders14/Main.java
@@ -0,0 +1,16 @@
+package ders14;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Product product1 = new Product("Telefon", 2000, 10);
+        Product product2 = new Product("Laptop", 2500, 5);
+        Product product3 = new Product("Kitab", 12, 3);
+
+        System.out.println(product1);
+        System.out.println(product2);
+        System.out.println(product3);
+
+	}
+
+}
---
+++ codes/javase/JavaSe/src/ders14/Product.java
@@ -0,0 +1,17 @@
+package ders14;
+
+public class Product {
+	private String name;
+    private double price;
+    private int quantity;
+    
+    public Product(String name, double price, int quantity) {
+    	this.name = name;
+    	this.price = price;
+    	this.quantity = quantity;
+    };
+    
+    public String toString() {
+    	return "Məhsulun adı: " + name + ", Qiyməti: " + price + " AZN, Sayı: " + quantity;
+    };
+}
