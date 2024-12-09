6d8fa05dc8d3f818881e306e724802acf74ed09e
mahammadov724 <nerimanmehemmedov@gmail.com>
2024-12-09 17:22:55 +0400

    toString

---
+++ codes/javase/JavaSe/src/toString/Main.java
@@ -0,0 +1,12 @@
+package toString;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Student student = new Student("Neriman", 14);
+
+        System.out.println(student);
+
+	}
+
+}
---
+++ codes/javase/JavaSe/src/toString/Student.java
@@ -0,0 +1,16 @@
+package toString;
+
+public class Student {
+	private String name;
+    private int age;
+
+    public Student(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "Tələbənin adı: " + name + ", Yaşı: " + age;
+    }
+}
