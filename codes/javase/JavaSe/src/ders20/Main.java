8d7230de5a1a09e32a3484b60cc8f246347392ef
mahammadov724 <nerimanmehemmedov@gmail.com>
2025-01-04 15:11:04 +0400

    Ders20

--- codes/javase/JavaSe/src/ders20/Main.java
+++ codes/javase/JavaSe/src/ders20/Main.java
@@ -4,7 +4,7 @@ import java.util.Map;
 public class Main {
 	public static void main(String[] args) {
 		Map<String, String> map = new HashMap<>();
-        map.put("Ad", "Aysel");
+        map.put("Ad", "Aygun");
         map.put("Şəhər", "Bakı");
         map.put("Dövlət", "Azərbaycan");
         
