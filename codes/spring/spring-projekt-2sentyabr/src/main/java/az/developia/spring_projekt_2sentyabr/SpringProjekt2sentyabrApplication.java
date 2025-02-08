f215364fffa8f5c02d993b6efeb309a613d48707
mahammadov724 <nerimanmehemmedov@gmail.com>
2025-02-08 16:48:15 +0400

    create-spring-project

---
+++ codes/spring/spring-projekt-2sentyabr/src/main/java/az/developia/spring_projekt_2sentyabr/SpringProjekt2sentyabrApplication.java
@@ -0,0 +1,13 @@
+package az.developia.spring_projekt_2sentyabr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringProjekt2sentyabrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringProjekt2sentyabrApplication.class, args);
+	}
+
+}
---
+++ codes/spring/spring-projekt-2sentyabr/src/test/java/az/developia/spring_projekt_2sentyabr/SpringProjekt2sentyabrApplicationTests.java
@@ -0,0 +1,13 @@
+package az.developia.spring_projekt_2sentyabr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringProjekt2sentyabrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
