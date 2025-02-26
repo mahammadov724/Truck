dca382279d87740f8babd613bbfeb1b9d9b04502
mahammadov724 <nerimanmehemmedov@gmail.com>
2025-02-26 22:05:19 +0400

    Praktika

--- codes/spring/spring-projekt-2sentyabr/src/main/java/az/developia/spring_projekt_2sentyabr/entity/Book.java
+++ codes/spring/spring-projekt-2sentyabr/src/main/java/az/developia/spring_projekt_2sentyabr/entity/Book.java
@@ -1,52 +1,27 @@
 package az.developia.spring_projekt_2sentyabr.entity;
 
 public class Book {
-		private int id;
-		private String name;
-		private double price;
-		private int pageCount;
-		
-		public Book() {
-			this.id = 230;
-			this.name = "Cinayət və Cəza";
-			this.price = 9.90;
-			this.pageCount = 512;
-		}
-		
-		public int getId() {
-	        return id;
-	    }
+    private String name;
+    private String author;
 
-	    public void setId(int id) {
-	        this.id = id;
-	    }
-		
-		public String getName() {
-	        return name;
-	    }
+    public Book(String name, String author) {
+        this.name = name;
+        this.author = author;
+    }
 
-	    public void setName(String name) {
-	        this.name = name;
-	    }
+    public String getName() {
+        return name;
+    }
 
-	    public double getPrice() {
-	        return price;
-	    }
+    public void setName(String name) {
+        this.name = name;
+    }
 
-	    public void setPrice(double price) {
-	        this.price = price;
-	    }
+    public String getAuthor() {
+        return author;
+    }
 
-	    public int getPageCount() {
-	        return pageCount;
-	    }
-
-	    public void setPageCount(int pageCount) {
-	        this.pageCount = pageCount;
-	    }
-	    
-	    @Override
-	    public String toString() {
-	        return "Book [id=" + id + ", name=" + name + ", price=" + price + ", pageCount=" + pageCount + "]";
-	    }
-	}
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+}
--- codes/spring/spring-projekt-2sentyabr/src/main/java/az/developia/spring_projekt_2sentyabr/entity/BookController.java
+++ codes/spring/spring-projekt-2sentyabr/src/main/java/az/developia/spring_projekt_2sentyabr/entity/BookController.java
@@ -1,11 +1,25 @@
 package az.developia.spring_projekt_2sentyabr.entity;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+
 
 @Controller
 public class BookController {
-	 @GetMapping("/books")
-	    public String getBooks() {
-	        return "books";  
-	    }
+
+    @GetMapping(path = "/kitablar")
+    public String showBooks(Model model) {
+        List<Book> books = new ArrayList<>();
+        books.add(new Book("Cinayet ve Ceza", "Fyodor Dostoyevski"));
+        books.add(new Book("Sefiller", "Victor Hugo"));
+        books.add(new Book("1984", "George Orwell"));
+
+        model.addAttribute("books", books);
+
+        return "books"; 
+    }
 }
+
