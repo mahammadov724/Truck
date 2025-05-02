65f6768dc3d774d9faa6eb07303c368820d6545a
mahammadov724 <nerimanmehemmedov@gmail.com>
2025-05-02 18:08:15 +0400

    Ders53

--- codes/spring/my-spring-project/src/main/java/codes/neriman/my_spring_project/service/BookService.java
+++ codes/spring/my-spring-project/src/main/java/codes/neriman/my_spring_project/service/BookService.java
@@ -1,11 +1,14 @@
 package codes.neriman.my_spring_project.service;
 
+import codes.neriman.my_spring_project.dto.BookRequestDto;
 import codes.neriman.my_spring_project.entity.Book;
 import codes.neriman.my_spring_project.exception.OurRunTimeException;
-import codes.neriman.my_spring_project.repository.BookRepository; 
+import codes.neriman.my_spring_project.repository.BookRepository;
+import codes.neriman.my_spring_project.repository.ReaderRepository;
 import codes.neriman.my_spring_project.responce.BookResponce;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -48,6 +51,21 @@ public class BookService {
                 .filter(book -> book.getTitle().toLowerCase().contains(query.toLowerCase()) || 
                                 book.getAuthor().toLowerCase().contains(query.toLowerCase())) 
                 .collect(Collectors.toList());
+    }
+    
+    public void add(BookRequestDto dto) {
+    	String username = SecurityContextHolder.getContext().getAuthentication().getName();
+    	Book book = new Book();
+    	book.setId(null);
+    	book.setAuthor(dto.getAuthor());
+    	book.setTitle(dto.getTitle());
+    	book.setYear(dto.getYear());
+    }
+    
+    public BookResponce get() {
+    	BookResponce responce = new BookResponce();
+    	responce.setBooks(bookRepository.findAll());
+    	return responce;
     }
 
 
